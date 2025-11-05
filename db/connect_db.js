c8db06b96fb98d2b8bfdbc5b1164bc505bb0fce3
Aneeq Ahmed <161975416+CodeAneeq@users.noreply.github.com>
2025-11-06 04:52:32 +0500

    Update connect_db.js

--- db/connect_db.js
+++ db/connect_db.js
@@ -2,7 +2,12 @@ import mongoose from "mongoose";
 
 export default async function connectDB(URI) {
     try {
-      await mongoose.connect(URI);
+      await mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 30000, 
+    socketTimeoutMS: 45000, 
+});
         console.log("Mongo DB Connected Successfully");
     } catch (error) {
         console.log("Mongo Db Failed", error);
